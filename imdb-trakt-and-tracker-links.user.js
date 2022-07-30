81eb973d9bec27187376d6275d87b4693882e75c
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-07-30 22:58:43 +0300

    replace with self invoking function

--- imdb-trakt-and-tracker-links.user.js
+++ imdb-trakt-and-tracker-links.user.js
@@ -4,23 +4,25 @@
 // @description      Links to torrents and trakt directly from imdb page
 // @license          MIT
 // @include          https://www.imdb.com/*
-// @version          2.3.9
+// @version          2.4.0
 // @updateURL        https://github.com/danielvigaru/imdb-trakt-and-tracker-links.user.js/raw/main/imdb-trakt-and-tracker-links.user.js
 // @downloadURL      https://github.com/danielvigaru/imdb-trakt-and-tracker-links.user.js/raw/main/imdb-trakt-and-tracker-links.user.js
 // @homepageURL      https://github.com/danielvigaru/imdb-trakt-and-tracker-links.user.js
 // @grant            none
 // ==/UserScript==
 
-const getIMDBid = () => {
-    const regexImdbNum = /\/title\/tt(\d{1,})/;
-    const id = regexImdbNum.exec(document.location)[1];
-    return id;
-};
+(function () {
+    function getIMDBid() {
+        const regexImdbNum = /\/title\/tt(\d{1,})/;
+        const id = regexImdbNum.exec(document.location)[1];
+        return id;
+    }
 
-window.onload = () => {
-    const movieId = getIMDBid();
+    function addLinks() {
+        const movieId = getIMDBid();
 
-    if (movieId) {
+        if (!movieId) return;
+
         const style = "style='text-decoration:none; color:white;' target='_blank'";
         const linkRarbg = `<a ${style} href='https://rarbgmirror.org/torrents.php?imdb=tt${movieId}'">RARBG</a>`;
         const linkFilelist = `<a ${style} href='https://filelist.io/browse.php?search=tt${movieId}'">FileList</a>`;
@@ -34,4 +36,6 @@ window.onload = () => {
         const banner = document.querySelector("[data-testid='hero-subnav-bar-left-block']");
         banner.prepend(links);
     }
-};
+
+    addLinks();
+})();
